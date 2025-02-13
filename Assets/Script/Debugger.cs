b863cbae1019b7b8e2f2833ad9fb0e71e36f49c7
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-13 22:52:24 +0900

    [add]デバッグ用のSOを追加

---
+++ Assets/Script/Debugger.cs
@@ -0,0 +1,34 @@
+using SymphonyFrameWork.CoreSystem;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Debugger")]
+public class Debugger : ScriptableObject
+{
+    public void PauseAndResume()
+    {
+        PauseManager.Pause = !PauseManager.Pause;
+    }
+
+
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(Debugger))]
+public class MyScriptEditor : Editor
+{
+    public override async void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.Space(10);
+        var storyData = target as Debugger;
+
+        if (GUILayout.Button("テキスト読み込み"))
+        {
+            storyData.PauseAndResume();
+        }
+    }
+}
+#endif
