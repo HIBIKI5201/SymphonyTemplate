e576a1b5deb34942a926cd2a3308820b148e24ea
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-14 21:18:59 +0900

    [update]ログを英語に

--- Assets/Script/SymphonyFrameWork/Utility/SymphonyTask.cs
+++ Assets/Script/SymphonyFrameWork/Utility/SymphonyTask.cs
@@ -11,7 +11,7 @@ namespace SymphonyFrameWork.Utility
         {
             await Awaitable.BackgroundThreadAsync();
             action.Invoke();
-            Debug.Log($"{action}????s???I?????????");
+            Debug.Log($"{action.Method} is done");
         }
 
         public static async Task WaitUntil(Func<bool> action, CancellationToken token)
