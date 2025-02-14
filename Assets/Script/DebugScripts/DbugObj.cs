e03b0de35f5da7c385d3d4fd1dfbe83c78ebc9b8
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-14 21:15:24 +0900

    [add]デバッグ用のオブジェクトを追加

---
+++ Assets/Script/DebugScripts/DbugObj.cs
@@ -0,0 +1,35 @@
+using SymphonyFrameWork.CoreSystem;
+using UnityEngine;
+
+public class DbugObj : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5;
+
+    Vector3 _velocity = default;
+
+    private void Start()
+    {
+        _velocity = Vector3.up * _speed;
+    }
+
+    void Update()
+    {
+        if (PauseManager.Pause)
+        {
+            return;
+        }
+
+        if (-5 > transform.position.y)
+        {
+            _velocity = Vector3.up * _speed;
+        }
+
+        if (5 < transform.position.y)
+        {
+            _velocity = Vector3.down * _speed;
+        }
+
+        transform.position += _velocity * Time.deltaTime;
+    }
+}
