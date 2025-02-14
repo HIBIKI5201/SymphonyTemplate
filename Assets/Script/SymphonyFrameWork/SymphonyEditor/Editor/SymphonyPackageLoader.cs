c314577f67f03dd644d9aafcea8e242ba3125893
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-15 03:15:58 +0900

    [update]ロード済みのパッケージを取得する

--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/SymphonyPackageLoader.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/SymphonyPackageLoader.cs
@@ -1,19 +1,73 @@
+using System.Linq;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
-namespace SymphonyFrameWork
+namespace SymphonyFrameWork.Editor
 {
-    public class SymphonyPackageLoader : MonoBehaviour
+    [InitializeOnLoad]
+    public static class SymphonyPackageLoader
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private static readonly string[] requirePackages = new string[]
         {
-        
-        }
+            "com.unity.addressables",
+        };
 
-        // Update is called once per frame
-        void Update()
+        static SymphonyPackageLoader()
         {
-        
+            ListRequest listRequest = Client.List();
+
+
+            if (listRequest.IsCompleted)
+            {
+                if (listRequest.Status == StatusCode.Failure)
+                {
+                    Debug.LogError("Failed to fetch package list: " + listRequest.Error.message);
+                    return;
+                }
+
+                // インストールされているパッケージの名前を取得
+                var installedPackages = listRequest.Result;
+                foreach (var packageName in requirePackages)
+                {
+                    var isPackageInstalled = installedPackages.Any(pkg => pkg.name == packageName);
+                    if (!isPackageInstalled)
+                    {
+                        // パッケージがインストールされていない場合、インストールを試みる
+                        //InstallPackage(packageName);
+
+                        Debug.Log($"is package installed {isPackageInstalled}");
+                    }
+                }
+
+                Debug.Log(string.Join(" ", installedPackages));
+            };
+
+            void InstallPackage(string name)
+            {
+                // パッケージをインストール
+                AddRequest addRequest = Client.Add(name);
+
+                // インストール処理後のコールバック
+                EditorApplication.update += () =>
+                {
+                    if (addRequest.IsCompleted)
+                    {
+                        if (addRequest.Status == StatusCode.Failure)
+                        {
+                            Debug.LogError($"Failed to install package: {addRequest.Error.message}");
+                        }
+                        else
+                        {
+                            Debug.Log($"Package installed: {name}");
+                        }
+
+                        // 次回更新での処理を停止
+                        EditorApplication.update -= () => { };
+                    }
+                };
+            }
         }
     }
 }
