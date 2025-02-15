d0a2cec62f25db1c5f2c7161f1ba661d4b77ae7b
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-15 19:56:48 +0900

    [update]シーンコンフィグにデータを追加

--- Assets/Script/SymphonyFrameWork/Configs/SceneManagerConfig.cs
+++ Assets/Script/SymphonyFrameWork/Configs/SceneManagerConfig.cs
@@ -7,6 +7,12 @@ namespace SymphonyFrameWork.Config
     /// </summary>
     public class SceneManagerConfig : ScriptableObject
     {
+        [SerializeField, Tooltip("ロードシーンを有効化するかどうか")]
+        private bool _isActiveLoadScene;
+        public bool IsActiveLoadScene { get => _isActiveLoadScene; }
 
+        [SerializeField, Tooltip("ロード中に表示されるシーン")]
+        private string _loadScene;
+        public string LoadScene { get => _loadScene; }
     }
 }
