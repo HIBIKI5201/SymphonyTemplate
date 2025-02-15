1bad0a1fc5038372f4d12b7120b44dcfdd300c22
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-15 10:28:21 +0900

    [update]DecoratorDrawerを使用してプロパティに依存しないように

--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Attribute/DisplayTextAttribute.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Attribute/DisplayTextAttribute.cs
@@ -12,14 +12,29 @@ namespace SymphonyFrameWork.Attribute
             Text = text;
         }
     }
-
     [CustomPropertyDrawer(typeof(DisplayTextAttribute))]
-    public class DisplayTextDrawer : PropertyDrawer
+    public class DisplayTextDecoratorDrawer : DecoratorDrawer
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        // 属性のインスタンスを取得するためのプロパティ
+        DisplayTextAttribute DisplayTextAttribute
         {
-            var displayTextAttribute = (DisplayTextAttribute)attribute;
-            EditorGUI.LabelField(position, displayTextAttribute.Text);
+            get { return (DisplayTextAttribute)attribute; }
+        }
+
+        // DecoratorDrawer は実際のフィールドの高さとは別に、描画領域の高さを指定できる
+        public override float GetHeight()
+        {
+            // 複数行に対応したい場合は、EditorStyles.label.CalcHeight() などで計算可能
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public override void OnGUI(Rect position)
+        {
+            position.x += position.width / 2;
+            position.width /= 2;
+
+            // 指定した領域にテキストを表示する
+            EditorGUI.LabelField(position, DisplayTextAttribute.Text);
         }
     }
 }
