d56da8a4d01a5f468c8e2b54e99324f341d7e969
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-15 18:44:27 +0900

    [add]シーンマネージャーコンフィグを追加

---
+++ Assets/Script/SymphonyFrameWork/Configs/SceneManagerConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SymphonyFrameWork.Config
+{
+    /// <summary>
+    /// シーンマネージャーのコンフィグを格納する
+    /// </summary>
+    public class SceneManagerConfig : ScriptableObject
+    {
+
+    }
+}
