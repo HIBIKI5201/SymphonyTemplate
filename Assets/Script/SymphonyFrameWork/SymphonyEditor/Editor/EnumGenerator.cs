80b62145c4ce731e5d69463370a9a096eb69f444
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-15 21:07:43 +0900

    [update]Enumの生成機構を追加

--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/EnumGenerator.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/EnumGenerator.cs
@@ -1,9 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
 
 namespace SymphonyFrameWork.Editor
 {
-    public class EnumGenerator
+    public static class EnumGenerator
     {
+        private const string FrameWork_Path = "\"Assets/Script/SymphonyFrameWork/";
+        private static async void Method(string[] strings, string fileName)
+        {
+            var enumFilePath = $"{FrameWork_Path}{fileName}.cs";
+            if (!File.Exists(enumFilePath))
+            {
+                File.Create(enumFilePath);
+            }
 
+            File.WriteAllText(enumFilePath, "", Encoding.UTF8);
+
+            IEnumerable<string> content = new[] { "public enum scenarioVoiceEnum : int {" };
+
+            content = content.Concat(strings);
+            content = content.Append("}");
+
+            await File.WriteAllLinesAsync(enumFilePath, content, Encoding.UTF8);
+        }
     }
 }
