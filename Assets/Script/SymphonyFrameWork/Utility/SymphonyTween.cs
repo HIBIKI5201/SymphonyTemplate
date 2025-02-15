67859df83bc86820811a6e5b2e36aa72087eb452
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-15 11:02:49 +0900

    [update]SymphonyTweenを追加

---
+++ Assets/Script/SymphonyFrameWork/Utility/SymphonyTween.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SymphonyFrameWork
+{
+    public class SymphonyTween : MonoBehaviour
+    {
+        public static async void Tweening()
+        {
+            return;
+        }
+    }
+}
