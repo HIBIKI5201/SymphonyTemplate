81840fb51eed304ece5890c566fd8aa28c195106
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-16 17:45:08 +0900

    [update]インデックスをEnumに含める

--- Assets/Script/SymphonyFrameWork/Enum/SceneListEnum.cs
+++ Assets/Script/SymphonyFrameWork/Enum/SceneListEnum.cs
@@ -1,7 +1,8 @@
-﻿public enum SceneListEnum : int {
-a,
-b,
-c,
-d,
-g,
+﻿public enum SceneListEnum : int
+{
+    a = 0,
+    b = 1,
+    c = 2,
+    d = 3,
+    g = 4,
 }
--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/EnumGenerator.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/EnumGenerator.cs
@@ -31,11 +31,7 @@ namespace SymphonyFrameWork.Editor
                 File.Delete(enumFilePath);
             }
 
-            //ファイルの中身を生成
-            IEnumerable<string> content = new[] { $"public enum " + fileName + "Enum : int {" };
-
-            content = content.Concat(hash.Select(s => $"{s},"));
-            content = content.Append("}");
+            var content = NormalEnumGenerate(fileName, hash);
 
             await File.WriteAllLinesAsync(enumFilePath, content, Encoding.UTF8);
         }
@@ -51,6 +47,24 @@ namespace SymphonyFrameWork.Editor
                 Directory.CreateDirectory(resourcesPath);
                 AssetDatabase.Refresh();
             }
+        }
+
+        /// <summary>
+        /// 通常のEnumを生成する
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="hash"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> NormalEnumGenerate(string fileName, HashSet<string> hash)
+        {
+            //ファイルの中身を生成
+            IEnumerable<string> content = new[] { $"public enum " + fileName + "Enum : int\n{" };
+
+            //Enumファイルに要素を追加していく
+            content = content.Concat(hash.Select((string s, int i) => $"    {s} = {i},"));
+            content = content.Append("}");
+
+            return content;
         }
     }
 }
