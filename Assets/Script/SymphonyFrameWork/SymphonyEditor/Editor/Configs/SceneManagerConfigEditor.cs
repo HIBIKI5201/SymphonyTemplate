c0e1ab9d683d47bf773c50e579af3495092fccdf
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-17 00:08:09 +0900

    [update]Partialクラスを使用してエディタ用の部分だけ設定

---
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SceneManagerConfigEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace SymphonyFrameWork.Config
+{
+    public partial class SceneManagerConfig
+    {
+        private void OnEnable()
+        {
+            _sceneList = EditorBuildSettings.scenes
+                .Select(s => Path.GetFileNameWithoutExtension(s.path))
+                .ToArray();
+        }
+    }
+}
