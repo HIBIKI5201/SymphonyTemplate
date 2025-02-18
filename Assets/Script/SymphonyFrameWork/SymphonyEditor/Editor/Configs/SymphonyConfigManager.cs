cf8ad75d7e60096408ec737f9cf7fa7196ce7e5b
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-18 22:58:17 +0900

    [update]エディタ用のコンフィグを生成できるように

--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SymphonyConfigManager.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SymphonyConfigManager.cs
@@ -13,43 +13,69 @@ namespace SymphonyFrameWork.Editor
     [InitializeOnLoad]
     public static class SymphonyConfigManager
     {
+        /// <summary>
+        /// ファイルがあるか確認する
+        /// </summary>
         static SymphonyConfigManager()
         {
-            FileCheck<SceneManagerConfig>();
+            FileCheck<SceneManagerConfig>(FileType.Runtime);
+            FileCheck<SymphonyWindowConfig>(FileType.Editor);
         }
 
-        private static void FileCheck<T>() where T : ScriptableObject
+        /// <summary>
+        /// ファイルがない場合は再生成する
+        /// </summary>
+        /// <param name="type"></param>
+        /// <typeparam name="T"></typeparam>
+        private static void FileCheck<T>(FileType type) where T : ScriptableObject
         {
-            //型の名前でパスを指定
-            string filePath = $"{SymphonyConstant.RESOURCES_PATH}/{typeof(T).Name}.asset";
-
-            // ファイルが存在しない場合
-            if (AssetDatabase.LoadAssetAtPath<T>(filePath) == null)
+            //パスを取得
+            string path = type switch
             {
-                CreateResourcesFolder();
+                FileType.Runtime => SymphonyConstant.RESOURCES_RUNTIME_PATH,
+                FileType.Editor => SymphonyConstant.RESOURCES_EDITOR_PATH,
+                _ => string.Empty
+            };
 
-                //対象のアセットを生成してResources内に配置
-                T asset = ScriptableObject.CreateInstance<T>();
-                AssetDatabase.CreateAsset(asset, filePath);
-                AssetDatabase.SaveAssets();
-
-                SymphonyDebugLog.DirectLog($"'{SymphonyConstant.RESOURCES_PATH}' に新しい {typeof(T).Name} を作成しました。");
+            if (string.IsNullOrEmpty(path))
+            {
+                Debug.LogWarning("SymphonyConfigManager: File not found: " + type.ToString());
+                return;
             }
+            
+            //型の名前でパスを指定
+            string filePath = $"{path}/{typeof(T).Name}.asset";
+            
+            if (AssetDatabase.LoadAssetAtPath<T>(filePath) != null) return;
+            
+            //フォルダがなければ生成
+            CreateResourcesFolder(path);
+
+            //対象のアセットを生成してResources内に配置
+            T asset = ScriptableObject.CreateInstance<T>();
+            AssetDatabase.CreateAsset(asset, filePath);
+            AssetDatabase.SaveAssets();
+
+            SymphonyDebugLog.DirectLog($"'{path}' に新しい {typeof(T).Name} を作成しました。");
         }
 
         /// <summary>
         /// リソースフォルダが無ければ生成
         /// </summary>
-        private static void CreateResourcesFolder()
+        private static void CreateResourcesFolder(string resourcesPath)
         {
-            string resourcesPath = SymphonyConstant.RESOURCES_PATH;
-
             //リソースがなければ生成
             if (!Directory.Exists(resourcesPath))
             {
                 Directory.CreateDirectory(resourcesPath);
                 AssetDatabase.Refresh();
             }
+        }
+
+        private enum FileType
+        {
+            Runtime,
+            Editor,
         }
     }
 }
