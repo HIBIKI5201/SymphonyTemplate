3fc71d4febd8891b1fbf94ff6fcd88250fece23e
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-18 22:38:09 +0900

    Revert "[update]名前空間の修正"
    This reverts commit 99ff4fe7ffa3289808dfb08a39dd4ff28b94a1ef.

--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SymphonyConfigManager.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SymphonyConfigManager.cs
@@ -1,7 +1,7 @@
-﻿using System.IO;
-using SymphonyFrameWork.Config;
+﻿using SymphonyFrameWork.Config;
 using SymphonyFrameWork.Core;
 using SymphonyFrameWork.Debugger;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
--- Assets/Script/SymphonyFrameWork/SymphonyEditor/SymphonyDebugLog.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/SymphonyDebugLog.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
