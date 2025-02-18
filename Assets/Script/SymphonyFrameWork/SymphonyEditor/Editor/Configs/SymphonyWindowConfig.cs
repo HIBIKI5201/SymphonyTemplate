4dced3f170f53cf053f346134f0378c662e145fb
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-18 22:52:30 +0900

    [update]シーンリスト更新コンフィグを移動

--- Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SymphonyWindowConfig.cs
+++ Assets/Script/SymphonyFrameWork/SymphonyEditor/Editor/Configs/SymphonyWindowConfig.cs
@@ -1,9 +1,11 @@
+using SymphonyFrameWork.Attribute;
 using UnityEngine;
 
 namespace SymphonyFrameWork.Editor
 {
     public class SymphonyWindowConfig : ScriptableObject
     {
-
+        [ReadOnly, SerializeField]
+        private bool _autoSceneListUpdate = true;
     }
 }
