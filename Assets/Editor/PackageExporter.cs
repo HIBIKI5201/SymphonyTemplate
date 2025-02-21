62bc64a05c9890f8d9487ee6fd5a940e56d7a592
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-22 02:59:37 +0900

    [update]リソースを含まないように

--- Assets/Editor/PackageExporter.cs
+++ Assets/Editor/PackageExporter.cs
@@ -1,21 +1,57 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using SymphonyFrameWork.Core;
 
 public class PackageExporter : EditorWindow
 {
-    [MenuItem("Tools/Export SymhpnyFrameWork Package")]
+    [MenuItem("Tools/Export SymphonyFrameWork Package")]
     public static void ExportPackage()
     {
-        // エクスポートしたいフォルダのパス
+        // エクスポート対象のフォルダ
         var folderPath = "Assets/Script/SymphonyFrameWork";
+
+        // 除外するリソース系のフォルダ
+        var excludePaths = new string[]
+        {
+            SymphonyConstant.ENUM_PATH,
+            SymphonyConstant.RESOURCES_RUNTIME_PATH,
+            SymphonyConstant.RESOURCES_EDITOR_PATH,
+        };
+
+        var parentPaths = new string[]
+        {
+            //親フォルダ
+            SymphonyConstant.FRAMEWORK_PATH,
+            SymphonyConstant.FRAMEWORK_PATH + "/Runtime",
+            SymphonyConstant.FRAMEWORK_PATH + "/SymphonyEditor",
+            SymphonyConstant.FRAMEWORK_PATH + "/SymphonyEditor/Editor",
+        };
+
+        // フォルダ内のすべてのアセットを取得
+        string[] assetGUIDs = AssetDatabase.FindAssets(string.Empty, new[] { folderPath });
+        List<string> assetPaths = assetGUIDs
+            .Select(AssetDatabase.GUIDToAssetPath)
+            //親フォルダを除外
+            .Where(path => parentPaths.All(parent => path != parent))
+            //リソース系フォルダに含まれているファイルを除外
+            .Where(path => !excludePaths.Any(exclude => path.StartsWith(exclude)))
+            .ToList();
+
+        if (assetPaths.Count == 0)
+        {
+            Debug.LogWarning("エクスポート対象のアセットがありません。");
+            return;
+        }
+        
+        Debug.Log(string.Join("\n", assetPaths));
 
         // 保存先のパス（エクスポート先）
         var exportPath = @"C:\Users\takut\OneDrive\デスクトップ\Sinfonia Studio\SymphonyFrameWork.unitypackage";
 
         // UnityPackageを作成
-        var files = new[] { folderPath };
-        AssetDatabase.ExportPackage(files, exportPath,
-            ExportPackageOptions.Recurse);
+        AssetDatabase.ExportPackage(assetPaths.ToArray(), exportPath, ExportPackageOptions.Recurse);
 
         Debug.Log("Package exported to: " + exportPath);
     }
