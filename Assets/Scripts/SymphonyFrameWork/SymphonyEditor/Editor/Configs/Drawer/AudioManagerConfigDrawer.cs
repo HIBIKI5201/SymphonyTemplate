a1aace7209aabbfbab231a37615ceaa50161b9ff
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-05 16:05:01 +0900

    [add]コンフィグのドロワーを追加しEnumを生成できるボタンを追加

---
+++ Assets/Scripts/SymphonyFrameWork/SymphonyEditor/Editor/Configs/Drawer/AudioManagerConfigDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SymphonyFrameWork.Editor
+{
+    [CustomEditor(typeof(AudioManagerConfig))]
+    public class AudioManagerConfigDrawer : UnityEditor.Editor
+    {
+        private const string AudioGroupTypeEnumName = "AudioGroupType";
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var myScript = target as AudioManagerConfig;
+            if (GUILayout.Button($"{AudioGroupTypeEnumName}Enumを再生成"))
+            {
+                EnumGenerator.EnumGenerate(Array.Empty<string>(), AudioGroupTypeEnumName);
+            }
+        }
+
+    }
+}
