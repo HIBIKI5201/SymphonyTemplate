4382ba91b8378b6a2c087b4bbe7904d0aeca032f
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-05 19:16:04 +0900

    [add]自動Enum生成のCSを追加

---
+++ Assets/Scripts/SymphonyFrameWork/SymphonyEditor/Editor/Administrator/UITK/CS/AutoEnumGeneratorWindow.cs
@@ -0,0 +1,20 @@
+﻿using SymphonyFrameWork.Utility;
+using System.Threading.Tasks;
+using UnityEngine.UIElements;
+
+namespace SymphonyFrameWork.Editor
+{
+    [UxmlElement]
+    public partial class AutoEnumGeneratorWindow : SymphonyVisualElement
+    {
+        public AutoEnumGeneratorWindow() : base(
+            SymphonyWindow.UITK_UXML_PATH + "AutoEnumGeneratorWindow.uxml",
+            InitializeType.None,
+            LoadType.AssetDataBase)
+        { }
+        protected override Task Initialize_S(TemplateContainer container)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
