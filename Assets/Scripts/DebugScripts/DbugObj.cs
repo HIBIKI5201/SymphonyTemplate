56fc957da516be9f5f19a76c9771fdac418c93df
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-05 16:25:39 +0900

    [update]デバッグで実行するように

--- Assets/Scripts/DebugScripts/DbugObj.cs
+++ Assets/Scripts/DebugScripts/DbugObj.cs
@@ -1,15 +1,15 @@
 ﻿using SymphonyFrameWork;
 using SymphonyFrameWork.Attribute;
 using SymphonyFrameWork.System;
-using SymphonyFrameWork.Utility;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class DbugObj : MonoBehaviour, IGameObject
 {
     [SerializeField] private float _speed = 5;
 
-    [DisplayText("試し書き試し書き試し書き")] [ReadOnly] [SerializeField]
+    [DisplayText("試し書き試し書き試し書き")]
+    [ReadOnly]
+    [SerializeField]
     private Vector3 _velocity;
 
     [SerializeField] private AnimationCurve _curve;
@@ -18,11 +18,7 @@ public class DbugObj : MonoBehaviour, IGameObject
     {
         _velocity = Vector3.up * _speed;
 
-        var renderer = GetComponent<MeshRenderer>();
-        Task task = SymphonyTween.PausableTweening(new Color(0, 0, 0), x => renderer.material.color = x, new Color(1, 1, 1), 10,
-            _curve).OnComplete(() => Debug.Log("Tween終了"));
-
-        ServiceLocator.GetInstance<DbugObj>();
+        AudioManager.GetAudioSource(AudioGroupTypeEnum.None);
     }
 
     private void Update()
