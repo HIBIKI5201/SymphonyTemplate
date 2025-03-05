2582b2774af3f92dfca4b6933bb943f6b6d371d6
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-05 19:04:45 +0900

    [update]自動Enum生成コンフィグにレイヤーとタグを追加

--- Assets/Scripts/SymphonyFrameWork/SymphonyEditor/Editor/Configs/AutoEnumGeneratorConfig.cs
+++ Assets/Scripts/SymphonyFrameWork/SymphonyEditor/Editor/Configs/AutoEnumGeneratorConfig.cs
@@ -1,16 +1,17 @@
-using SymphonyFrameWork.Attribute;
+﻿using SymphonyFrameWork.Attribute;
 using UnityEngine;
 
 namespace SymphonyFrameWork.Editor
 {
     public class AutoEnumGeneratorConfig : ScriptableObject
     {
-        [ReadOnly] [SerializeField] private bool _autoSceneListUpdate = true;
+        [ReadOnly, SerializeField] private bool _autoSceneListUpdate = true;
+        public bool AutoSceneListUpdate { get => _autoSceneListUpdate; set => _autoSceneListUpdate = value; }
 
-        public bool AutoSceneListUpdate
-        {
-            get => _autoSceneListUpdate;
-            set => _autoSceneListUpdate = value;
-        }
+        [ReadOnly, SerializeField] private bool _autoTagsUpdate = false;
+        public bool AutoTagsUpdate { get => _autoTagsUpdate; set => _autoTagsUpdate = value; }
+
+        [ReadOnly, SerializeField] private bool _autoLayersUpdate = false;
+        public bool AutoLayerUpdate { get => _autoLayersUpdate; set => _autoLayersUpdate = value; }
     }
 }
