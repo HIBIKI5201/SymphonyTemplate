c18de2bb26f67d7a374a5814f7669b1b703bb5b6
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-05 14:57:05 +0900

    [add]オーディオマネージャーのコンフィグを追加

---
+++ Assets/Scripts/SymphonyFrameWork/Runtime/Configs/AudioManagerConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace SymphonyFrameWork
+{
+    public class AudioManagerConfig : ScriptableObject
+    {
+        [SerializeField]
+        private AudioMixer _audioMixer;
+        public AudioMixer AudioMixer { get => _audioMixer; }
+    }
+}
