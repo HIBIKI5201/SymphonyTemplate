7407bc322672de418089e9639746366650fc0f51
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-05 16:07:00 +0900

    [update]コンフィグにグループの情報を入れられるように

--- Assets/Scripts/SymphonyFrameWork/Runtime/Configs/AudioManagerConfig.cs
+++ Assets/Scripts/SymphonyFrameWork/Runtime/Configs/AudioManagerConfig.cs
@@ -1,5 +1,9 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Experimental.GlobalIllumination;
 
 namespace SymphonyFrameWork
 {
@@ -8,5 +12,21 @@ namespace SymphonyFrameWork
         [SerializeField]
         private AudioMixer _audioMixer;
         public AudioMixer AudioMixer { get => _audioMixer; }
+
+        [SerializeField]
+        private List<AudioGroupSettings> _audioGroupSettingList;
+        public List<AudioGroupSettings> AudioGroupSettingList { get => _audioGroupSettingList; }
+
+        [Serializable]
+        public class AudioGroupSettings
+        {
+            [SerializeField]
+            private string _audioGroupName;
+            public string AudioGroupName { get => _audioGroupName; }
+
+            [SerializeField]
+            private bool _isLoop;
+            public bool IsLoop { get => _isLoop; }
+        }
     }
 }
