238cb0590a57be990598f6b13c604e685e743d3c
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-08 02:27:10 +0900

    [update]SMBシステムを追加

---
+++ Assets/Scripts/DebugScripts/SMBReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class SMBReceiver : MonoBehaviour
+{
+    public event Action<string, Phase> OnREceiveSignal;
+
+    public void ReceiveSignal(string name, Phase phase)
+    {
+        Debug.Log($"name : {name}, phase : {phase}");
+        OnREceiveSignal?.Invoke(name, phase);
+    }
+
+    public enum Phase
+    {
+        None,
+        Enter,
+        Update,
+        Exit,
+    }
+}
---
+++ Assets/Scripts/DebugScripts/SMBTransmitter.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class SMBTransmitter : StateMachineBehaviour
+{
+    private SMBReceiver _receiver = default;
+
+    [SerializeField]
+    private string _stateName = string.Empty;
+
+    [SerializeField]
+    private bool _enter;
+    [SerializeField]
+    private bool _update;
+    [SerializeField]
+    private bool _exit;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_enter)
+        {
+            StateEvent(animator, stateInfo, SMBReceiver.Phase.Enter);
+        }
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_update)
+        {
+            StateEvent(animator, stateInfo, SMBReceiver.Phase.Update);
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_exit)
+        {
+            StateEvent(animator, stateInfo, SMBReceiver.Phase.Exit);
+        }
+    }
+
+    /// <summary>
+    /// ステートのイベント
+    /// </summary>
+    private void StateEvent(Animator animator, AnimatorStateInfo info, SMBReceiver.Phase state)
+    {
+        if (string.IsNullOrEmpty(_stateName))
+        {
+            _stateName = "empty state name";
+        }
+
+        SendSignal(_stateName, state, animator);
+    }
+
+    /// <summary>
+    /// Receiverにイベントを送る
+    /// </summary>
+    /// <param name="stateName"></param>
+    /// <param name="state"></param>
+    /// <param name="animator"></param>
+    private void SendSignal(string stateName, SMBReceiver.Phase state, Animator animator)
+    {
+        //Receiverが無ければ取得・追加する
+        if (!_receiver)
+        {
+            _receiver = animator.GetComponent<SMBReceiver>();
+            if (!_receiver)
+            {
+                _receiver = animator.gameObject.AddComponent<SMBReceiver>();
+            }
+        }
+
+        _receiver.ReceiveSignal(stateName, state);
+    }
+}
