ad6e3ed78adbc4a2d810abd1469a27e801306225
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-08 02:37:55 +0900

    [add]エディタ用のConstrantクラスを追加

--- Assets/Scripts/SymphonyFrameWork/Core/SymphonyConstant.cs
+++ Assets/Scripts/SymphonyFrameWork/Core/SymphonyConstant.cs
@@ -10,22 +10,5 @@
         public const string ENUM_PATH = FRAMEWORK_PATH + "/Runtime/Enum";
 
         public const string MENU_PATH = "Window/Symphony FrameWork/";
-
-
-        public class EditorSymphonyConstrant
-        {
-#if UNITY_EDITOR
-            public const string ServiceLocatorSetInstanceKey = "ServiceLocatorSetInstanceLog";
-            public const bool ServiceLocatorSetInstanceDefault = true;
-            public const string ServiceLocatorGetInstanceKey = "ServiceLocatorGetInstanceLog";
-            public const bool ServiceLocatorGetInstanceDefault = false;
-            public const string ServiceLocatorDestroyInstanceKey = "ServiceLocatorDestroyInstanceLog";
-            public const bool ServiceLocatorDestroyInstanceDefault = true;
-
-            public const string SceneListEnumFileName = "SceneList";
-            public const string TagsEnumFileName = "Tags";
-            public const string LayersEnumFileName = "Layers";
-#endif
-        }
     }
 }
