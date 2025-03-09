726122c2ed74b3b036502da64e71b2f1416028aa
HIBIKI_5201 <takuto5201@gmail.com>
2025-03-10 04:17:27 +0900

    [fix]パスを修正

--- Assets/SymphonyFrameWork/Core/SymphonyConstant.cs
+++ Assets/SymphonyFrameWork/Core/SymphonyConstant.cs
@@ -2,7 +2,7 @@
 {
     public static class SymphonyConstant
     {
-        public const string SYMPHONY_PACKAGE = "symphony.framework";
+        public const string SYMPHONY_PACKAGE = "symphony framework";
         public const string SYMPHONY_FRAMEWORK = "SymphonyFrameWork";
 
         public const string RESOURCES_RUNTIME_PATH = "Assets/Resources/" + SYMPHONY_FRAMEWORK;
