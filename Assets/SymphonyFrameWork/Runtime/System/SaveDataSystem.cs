749462e4bc09e1c24957ad50f96b4e644560471d
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-23 02:07:26 +0900

    [update]幾つかコメントとコード規定に従ったクリーンアップ

--- Assets/SymphonyFrameWork/Runtime/System/SaveDataSystem.cs
+++ Assets/SymphonyFrameWork/Runtime/System/SaveDataSystem.cs
@@ -10,8 +10,6 @@ namespace SymphonyFrameWork.System
     /// <typeparam name="DataType">データの型</typeparam>
     public static class SaveDataSystem<DataType> where DataType : class, new()
     {
-        private static SaveData _saveData;
-
         public static DataType Data
         {
             get
@@ -32,20 +30,31 @@ namespace SymphonyFrameWork.System
             }
         }
 
+        private static SaveData _saveData;
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Initialize()
         {
             _saveData = null;
         }
 
+        /// <summary>
+        ///     saveDataを保存する
+        /// </summary>
         public static void Save()
         {
-            _saveData = new SaveData(Data);
+            _saveData = new SaveData(Data); //今のデータでセーブデータを作る
+
+            //Json化してセーブ
             var data = JsonConvert.SerializeObject(_saveData);
-            Debug.Log($"{_saveData.SaveDate}\n{data}");
             PlayerPrefs.SetString(typeof(DataType).Name, data);
+
+            Debug.Log($"[{nameof(SaveDataSystem<DataType>)}]\nデータをセーブしました date : {_saveData.SaveDate}\n{data}");
         }
 
+        /// <summary>
+        ///     DataTypeのデータを取得する
+        /// </summary>
         private static void Load()
         {
             #region Prefsからデータをロードする
@@ -53,7 +62,7 @@ namespace SymphonyFrameWork.System
             var json = PlayerPrefs.GetString(typeof(DataType).Name);
             if (string.IsNullOrEmpty(json))
             {
-                Debug.Log($"{typeof(DataType).Name}のデータが見つからないので生成しました");
+                Debug.Log($"[{nameof(SaveDataSystem<DataType>)}]\n{typeof(DataType).Name}のデータが見つからないので生成しました");
                 _saveData = new SaveData(new DataType());
                 return;
             }
@@ -65,13 +74,13 @@ namespace SymphonyFrameWork.System
             var data = JsonConvert.DeserializeObject<SaveData>(json);
             if (data is not null)
             {
-                Debug.Log($"{typeof(DataType).Name}のデータがロードされました\n{data}");
+                Debug.Log($"[{nameof(SaveDataSystem<DataType>)}]\n{typeof(DataType).Name}のデータがロードされました\n{data}");
                 _saveData = data;
             }
             else
             {
-                Debug.LogWarning($"{typeof(DataType).Name}のロードが出来ませんでした");
-                _saveData = new SaveData(new DataType());
+                Debug.LogWarning($"[{nameof(SaveDataSystem<DataType>)}]\n{typeof(DataType).Name}のロードが出来ませんでした\n新たなインスタンスを生成します");
+                _saveData = new SaveData(new DataType()); //新しいデータを取得
             }
 
             #endregion
@@ -80,14 +89,17 @@ namespace SymphonyFrameWork.System
         [Serializable]
         private class SaveData
         {
-            public string SaveDate;
-            public DataType MainData;
-
             public SaveData(DataType dataType)
             {
-                SaveDate = DateTime.Now.ToString("O");
-                MainData = dataType;
+                _saveDate = DateTime.Now.ToString("O");
+                _mainData = dataType;
             }
+
+            public string SaveDate => _saveDate;
+            public DataType MainData => _mainData;
+
+            public readonly string _saveDate;
+            private readonly DataType _mainData;
         }
     }
 }
