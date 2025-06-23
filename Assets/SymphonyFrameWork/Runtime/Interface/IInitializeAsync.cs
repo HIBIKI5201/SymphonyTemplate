d9fdfe6c9eff0ba4b2c48dd7ca0691eb8f53af10
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-23 21:40:17 +0900

    [update]インターフェースを実装

--- Assets/SymphonyFrameWork/Runtime/Interface/IInitializeAsync.cs
+++ Assets/SymphonyFrameWork/Runtime/Interface/IInitializeAsync.cs
@@ -1,19 +1,33 @@
-using UnityEngine;
+﻿using System.Threading.Tasks;
 
 namespace SymphonyFrameWork
 {
-    public class IInitializeAsync : MonoBehaviour
+    /// <summary>
+    ///     非同期で初期化するインターフェース
+    /// </summary>
+    public interface IInitializeAsync
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public Task InitializeTask { get; protected set; }
+        public bool IsDone => InitializeTask != null ? InitializeTask.IsCompleted : false;
+
+        /// <summary>
+        ///     初期化を開始する
+        /// </summary>
+        /// <returns></returns>
+        public async Task DoInitialize()
         {
-        
+            if (IsDone) return; //再初期化をブロック
+
+            InitializeTask = InitializeAsync();
+            await InitializeTask;
+
+            InitializeTask = Task.CompletedTask; //軽量なTaskに変更
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        /// <summary>
+        ///     初期化のフローを実装する
+        /// </summary>
+        /// <returns></returns>
+        protected Task InitializeAsync();
     }
 }
